e6a7edc14d84a9c6147a37ed53661e5bc297f80a
Sarah Snyder <slsnyder@uw.edu>
2017-03-17 01:09:56 -0700

    Save / Load implemented with keypresses.

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
 
 var socket = io.connect("http://76.28.150.193:8888");
-var gameState;
+var gameState = INIT_STATE;
 var gameEngine;
 
 socket.on("load", function (data) {
-    console.log("load?");
+    console.log("load");
     console.log(data);
     gameState = data;
+    initialize();
 });
-
-socket.emit("load", {studentname: "slsnyder", statename: "cgolstate"});
 
 window.onload = function () {
     socket.on("connect", function () {
@@ -26,10 +25,60 @@ window.onload = function () {
 window.addEventListener("keydown", function (e) {
         console.log(e);
         console.log("Key Down Event - Char " + e.code + " Code " + e.keyCode);
-		if (String.fromCharCode(e.which) === 's') {
+		if (String.fromCharCode(e.which) === 'S') {
+            console.log("if S");
             socket.emit("save", {studentname: "slsnyder", statename: "cgolstate", data: gameEngine.getGameState()});
         }
+        
+        if (String.fromCharCode(e.which) === 'L') {
+            console.log("if L");
+            socket.emit("load", {studentname: "slsnyder", statename: "cgolstate"});
+        }
     }, false);
+    
+function initialize() {
+    var canvas = document.getElementById('gameWorld');
+    var ctx = canvas.getContext('2d');
+
+    gameEngine = new GameEngine(INIT_STATE);
+    
+    var allCells = new Array();
+    for (var i = 0; i < (CANVAS_H / CELL_H); i++){
+        allCells.push(new Array());
+    }
+    
+    //create cells
+    for (var i = 0; i < (CANVAS_H / CELL_H); i++) {
+        for (var j = 0; j < (CANVAS_W / CELL_W); j++) {
+            var cell = new Cell(gameEngine, i, j);
+            allCells[i].push(cell);
+            gameEngine.addEntity(cell);
+        }
+    }
+    
+    //set neighbors
+    for (var i = 0; i < (CANVAS_H / CELL_H); i++) {
+        for (var j = 0; j < (CANVAS_W / CELL_W); j++) {
+            
+            for (var m = -1; m <= 1; m++) {
+                for (var n = -1; n <= 1; n++) {
+                    var y = i + m;
+                    var x = j + n;
+                    
+                    // if the cell at x,y is in bounds and is not this cell
+                    if(y >= 0 && y < (CANVAS_H / CELL_H) &&
+                        x >= 0 && x < (CANVAS_W / CELL_W) &&
+                        (y != i || x != j) )
+                            allCells[i][j].neighbors.push(allCells[y][x]);
+                }
+            }
+            
+        }
+    }
+    
+    gameEngine.init(ctx);
+    gameEngine.start();
+}
 
 function Cell(game, yIndex, xIndex) {
     this.cellTimer = 0;
@@ -85,46 +134,5 @@ ASSET_MANAGER.queueDownload("./img/bg.jpg");
 
 ASSET_MANAGER.downloadAll(function () {
     console.log("starting up");
-    var canvas = document.getElementById('gameWorld');
-    var ctx = canvas.getContext('2d');
-
-    gameEngine = new GameEngine(INIT_STATE);
-    //gameEngine = new GameEngine(gameState);
-    
-    var allCells = new Array();
-    for (var i = 0; i < (CANVAS_H / CELL_H); i++){
-        allCells.push(new Array());
-    }
-    
-    //create cells
-    for (var i = 0; i < (CANVAS_H / CELL_H); i++) {
-        for (var j = 0; j < (CANVAS_W / CELL_W); j++) {
-            var cell = new Cell(gameEngine, i, j);
-            allCells[i].push(cell);
-            gameEngine.addEntity(cell);
-        }
-    }
-    
-    //set neighbors
-    for (var i = 0; i < (CANVAS_H / CELL_H); i++) {
-        for (var j = 0; j < (CANVAS_W / CELL_W); j++) {
-            
-            for (var m = -1; m <= 1; m++) {
-                for (var n = -1; n <= 1; n++) {
-                    var y = i + m;
-                    var x = j + n;
-                    
-                    // if the cell at x,y is in bounds and is not this cell
-                    if(y >= 0 && y < (CANVAS_H / CELL_H) &&
-                        x >= 0 && x < (CANVAS_W / CELL_W) &&
-                        (y != i || x != j) )
-                            allCells[i][j].neighbors.push(allCells[y][x]);
-                }
-            }
-            
-        }
-    }
-    
-    gameEngine.init(ctx);
-    gameEngine.start();
+    initialize();
 });
